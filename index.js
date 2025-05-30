8f7e595ead3dafcded8db500bc48249eac711fb8
Stephan Nufer <Stephan.Nufer@gmx.de>
2025-05-30 20:46:20 +0200

    This  commit add the ground structure of js with the questions.

---
+++ index.js
@@ -0,0 +1,67 @@
+//// TODO: Fragen Struktur:
+const questions = [
+  {
+    id: 1,
+    question: "Was ist die Hauptstadt von Deutschland?",
+    answers: [
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "b",
+        text: "Berlin",
+        correct: truee,
+      },
+      {
+        id: "c",
+        text: "Hamburg",
+        correct: false,
+      },
+      {
+        id: "d",
+        text: "Hannover",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Was ist die Hauptstadt von Frankreich?",
+    answers: [
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "b",
+        text: "Berlin",
+        correct: true,
+      },
+      {
+        id: "c",
+        text: "Hamburg",
+        correct: false,
+      },
+      {
+        id: "d",
+        text: "Hannover",
+        correct: false,
+      },
+    ],
+  },
+];
+
+// TODO 11: Frage Rendern;
+
+//TODO 12: "Next"Logik
+//
+// //TODO Frage beantworten mit Logik
+
+// TODO 14: Lösung anzeigen
+//
+//
+//  TODO 10: Fragen
+c;
